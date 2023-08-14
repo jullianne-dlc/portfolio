d130ed0bae7970920a244a715ac0d534bc8b6a71
JullianneDLC <julliannerosedelacruz@gmail.com>
2023-08-14 13:32:36 -0700

    hopefully menu button works on mobile now
    with chatGPT's guidance hopefully the menu button is better on mobile now omg. started using javascript, it saved me with the fade-ins now I hope it works with this too.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // JavaScript source code
 
+
+//for fading in images with div  fadein
 document.addEventListener("DOMContentLoaded", function() {
   const fadeDivs = document.querySelectorAll(".image.fadein");
 
@@ -9,5 +11,23 @@ document.addEventListener("DOMContentLoaded", function() {
     img.addEventListener("load", function() {
       div.classList.add("fadein-active");
     });
+  });
+});
+
+
+//for fixing menu button functionality on mobile
+document.addEventListener("DOMContentLoaded", function() {
+  const menuButton = document.getElementById("menu-button");
+  const menu = document.querySelector(".menu");
+
+  menuButton.addEventListener("click", function() {
+    menu.classList.toggle("menu-button-active");
+  });
+
+  // Close the dropdown menu when clicking outside
+  document.addEventListener("click", function(event) {
+    if (!menu.contains(event.target)) {
+      menu.classList.remove("menu-button-active");
+    }
   });
 });

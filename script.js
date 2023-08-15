ffddd89a2a26a5193a5c306867a7d05ee7318d19
JullianneDLC <julliannerosedelacruz@gmail.com>
2023-08-15 11:59:05 -0700

    reverting to the original fadein that kinda worked
    this one was the one that faded in on first load then no more after. that's okay tbh.

--- script.js
+++ script.js
@@ -1,36 +1,27 @@
 // JavaScript source code
 
 
-        // Function to apply the fade-in effect
-        const applyFadeIn = function(div) {
-          const img = div.querySelector("img");
-          if (img.complete) {
-            div.classList.add("fadein-active");
-          }
-        };
+    document.addEventListener("DOMContentLoaded", function() {
+      // Function to apply the fade-in effect
+      const applyFadeIn = function(div) {
+        const img = div.querySelector("img");
+        if (img.complete) {
+          div.classList.add("fadein-active");
+        }
+      };
 
-        // Apply fade-in effect when the page loads
-        document.addEventListener("DOMContentLoaded", function() {
-          const fadeDivs = document.querySelectorAll(".image.fadein");
-          fadeDivs.forEach(function(div) {
-            applyFadeIn(div);
+      // Fade-in effect for images with div.fadein
+      const fadeDivs = document.querySelectorAll(".image.fadein");
 
-            const img = div.querySelector("img");
-            img.addEventListener("load", function() {
-              applyFadeIn(div);
-            });
-          });
+      fadeDivs.forEach(function(div) {
+        applyFadeIn(div);
+
+        const img = div.querySelector("img");
+        img.addEventListener("load", function() {
+          applyFadeIn(div);
         });
-
-        // Apply fade-in effect again when the page becomes active
-        document.addEventListener("visibilitychange", function() {
-          if (!document.hidden) {
-            const fadeDivs = document.querySelectorAll(".image.fadein");
-            fadeDivs.forEach(function(div) {
-              applyFadeIn(div);
-            });
-          }
-        });
+      });
+    });
 
 
 });
